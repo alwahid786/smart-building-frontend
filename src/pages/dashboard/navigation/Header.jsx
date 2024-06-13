528133d3d1a6e37513ce69f613c4bea20809bb66
blocksgenie <ahmedcodes48@gmail.com>
2024-06-13 18:22:50 +0500

    after conflictsss resolved

--- src/pages/dashboard/navigation/Header.jsx
+++ src/pages/dashboard/navigation/Header.jsx
@@ -41,10 +41,8 @@ const Header = () => {
         open={openNav}
         onClose={() => toggleNav(false)}
         sx={{ width: '174px' }}
-      
+      >
         <Aside toggleNav={toggleNav} />
-
-       
       </Drawer>
       {/* </HeaderBg> */}
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
 
   return (
     <>
-      {/ {isLoading && <GlobalLoader />} /}
+      {/* {isLoading && <GlobalLoader /> */}
       <Box
         maxWidth="false"
         sx={{
