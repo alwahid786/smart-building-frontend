820ce3a24be664c8ea2b57c2242cc011a3277dd3
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-07-26 11:03:24 +0500

    se

--- src/pages/dashboard/sensors/Sensors.jsx
+++ src/pages/dashboard/sensors/Sensors.jsx
@@ -15,7 +15,6 @@ import { useGetAllSensorsQuery,  useCreateSensorMutation } from '../../../redux/
 import AddSensor from './AddSensor';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { Link, useMatch } from 'react-router-dom';
-import ViewSensor from './ViewSensor';
 
 
 const Sensors = () => {
