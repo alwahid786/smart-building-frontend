b84af199850edb03cece879b9080877ae36467ba
blocksgenie <ahmedcodes48@gmail.com>
2024-08-02 12:46:27 +0500

    sensor slidin fixed

--- src/pages/dashboard/buildinginfo/Components/BuildingDetails/components/Sensor.jsx
+++ src/pages/dashboard/buildinginfo/Components/BuildingDetails/components/Sensor.jsx
@@ -31,7 +31,7 @@ const Sensor = ({ sensors }) => {
     if (sensors && sensors.length > 0) {
       console.log('Sensors prop:', sensors)
       // Assuming sensors is an array of sensor objects
-      const sensorData = sensors.map(sensor => sensor.sensors).flat()
+      const sensorData = sensors.map((sensor) => sensor.sensors).flat()
       console.log('Mapped data:', sensorData)
       setData(sensorData)
     }
@@ -111,24 +111,33 @@ const Sensor = ({ sensors }) => {
             <Divider />
 
             <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                marginBottom: 1,
-                marginTop: 4,
-              }}
+              // sx={{
+              //   display: 'flex',
+              //   // justifyContent: 'space-between',
+              //   alignItems: 'center',
+              //   marginBottom: 1,
+              //   marginTop: 4,
+              //   border: '2px solid red',
+              // }}
+              sx={{ position: 'relative', display: 'flex' }}
             >
               <ArrowBackIosNewIcon
-                sx={{ cursor: 'pointer', mb: '250px' }}
+                sx={{
+                  cursor: 'pointer',
+                  mb: '250px',
+                  position: 'absolute',
+                  top: '12px',
+                  left: '10px',
+                  zIndex: '7',
+                }}
                 onClick={handlePrev}
               />
               <Box
                 className={animationClass}
                 sx={{
+                  marginTop: '20px',
                   display: 'flex',
                   flexDirection: 'column',
-                  alignItems: 'center',
                   width: '100%',
                 }}
               >
@@ -286,7 +295,14 @@ const Sensor = ({ sensors }) => {
                 </Box>
               </Box>
               <ArrowForwardIosIcon
-                sx={{ cursor: 'pointer', mb: '250px' }}
+                sx={{
+                  cursor: 'pointer',
+                  mb: '250px',
+                  position: 'absolute',
+                  top: '12px',
+                  right: '10px',
+                  zIndex: '7',
+                }}
                 onClick={handleNext}
               />
             </Box>
